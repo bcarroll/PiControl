22fe5af16a9b889bce12e66d9c9fded64c781714
Brett Carroll <brettwcarroll@hotmail.com>
2018-01-07 15:42:37 -0500

    error handling

--- lib/database_utils.py
+++ lib/database_utils.py
@@ -1,14 +1,15 @@
 import sqlite3
 from sqlite3 import Error
 from time import time
-import logging
 import datetime
 from pprint import pprint
 from flask import jsonify
+#import logging
 
+from lib._logging import logger
 from lib.pi_utilities import pi_model
 
-logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s')
+#logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s')
 
 def create_database(database_file='db/PiControl.db'):
     '''
@@ -26,7 +27,7 @@ def create_database(database_file='db/PiControl.db'):
         # Close the database connection
         conn.close()
     except Error as e:
-        logging.error(e)
+        logger.error(e)
 
 def create_tables(connection):
     '''
@@ -41,7 +42,7 @@ def create_tables(connection):
         cursor.execute("CREATE TABLE IF NOT EXISTS 'config' ('id' TEXT NOT NULL, 'beacon_port' INTEGER NOT NULL, 'beacon_interval' INTEGER NOT NULL, 'secret_key' TEXT NOT NULL, 'log_level' INTEGER NOT NULL, 'log_file' TEXT NOT NULL)")
         create_config(cursor)
     except Error as e:
-        logging.error(e)
+        logger.error(e)
 
 def create_config(cursor):
     '''
@@ -59,17 +60,17 @@ def create_config(cursor):
     try:
         # If the config table already has data, we don't need to do anything
         t = config_rows[0]
-        logging.debug('config table previously initialized.')
+        logger.debug('config table previously initialized.')
     except:
         # No results.  Add the default configuration data
-        logging.info('Adding default configuration to the PiControl database.')
+        logger.info('Adding default configuration to the PiControl database.')
         try:
             cursor.execute("INSERT INTO 'config' ('id', 'beacon_port', 'beacon_interval', 'secret_key', 'log_level', 'log_file') VALUES ('active', 31415, 60, 'PiControl', 30, 'logs/PiControl.log')")
         except Error as (e):
-            logging.error('Error adding default configuration to PiControl database. ' + str(e))
+            logger.error('Error adding default configuration to PiControl database. ' + str(e))
 
 def update_node(ipaddress, hostname, revision, serialnumber, last_checkin, database_file='db/PiControl.db'):
-    logging.debug("Updating node: ipaddress=" + str(ipaddress) + ", hostname=" + str(hostname) + ", revision=" + str(revision) + ", serialnumber=" + str(serialnumber) +", last_checkin=" + str(last_checkin))
+    logger.debug("Updating node: ipaddress=" + str(ipaddress) + ", hostname=" + str(hostname) + ", revision=" + str(revision) + ", serialnumber=" + str(serialnumber) +", last_checkin=" + str(last_checkin))
     try:
         conn = sqlite3.connect(database_file)
         cursor = conn.cursor()
@@ -78,44 +79,44 @@ def update_node(ipaddress, hostname, revision, serialnumber, last_checkin, datab
             results = cursor.fetchone()
             if results:
                 try:
-                    logging.debug('Updating node (' + hostname + ':' + ipaddress + ') in nodes table.')
+                    logger.debug('Updating node (' + hostname + ':' + ipaddress + ') in nodes table.')
                     cursor.execute('UPDATE nodes set ipaddress={0}, hostname={1}, revision={2}, serialnumber={3}, last_checkin={4} WHERE ipaddress={0}'.format("'{}'".format(ipaddress), "'{}'".format(hostname), "'{}'".format(revision), "'{}'".format(serialnumber), last_checkin))
                 except Error as (e):
-                    logging.debug(str(e))
+                    logger.debug(str(e))
                     return()
             else:
                 try:
-                    logging.debug('Node (' + hostname + ':' + ipaddress + ') does not exist in nodes table.  Inserting...')
+                    logger.debug('Node (' + hostname + ':' + ipaddress + ') does not exist in nodes table.  Inserting...')
                     cursor.execute('INSERT INTO nodes (ipaddress, hostname, revision, serialnumber, last_checkin) VALUES ({0},{1},{2},{3},{4})'.format("'{}'".format(ipaddress), "'{}'".format(hostname), "'{}'".format(revision), "'{}'".format(serialnumber), last_checkin))
                 except Error as (e):
-                    logging.error(e)
+                    logger.error(e)
                     return()
         except Error as (e):
-            logging.error(e)
+            logger.error(e)
             return()
         # Commit changes
         conn.commit()
         # Close the database connection
         conn.close()
     except Error as e:
-        logging.error(e)
+        logger.error(e)
         return()
 
 def update_config(beacon_port, beacon_interval, secret_key, log_level, log_file, database_file='db/PiControl.db'):
-    logging.debug('Updating config: beacon_port=' + str(beacon_port) + ', beacon_interval=' + str(beacon_interval) + ', secret_key=' + str(secret_key) + ', log_level=' + str(log_level) + ', log_file=' + str(log_file))
+    logger.debug('Updating config: beacon_port=' + str(beacon_port) + ', beacon_interval=' + str(beacon_interval) + ', secret_key=' + str(secret_key) + ', log_level=' + str(log_level) + ', log_file=' + str(log_file))
     try:
         conn = sqlite3.connect(database_file)
         cursor = conn.cursor()
         try:
             cursor.execute('UPDATE config set (beacon_port={0}, beacon_interval={1}, secret_key={2}, log_level={3}, log_file={4}) WHERE id="active"'.format(beacon_port, beacon_interval, "'{}'".format(secret_key), log_level, "'{}'".format(log_file)))
         except Error as (e):
-            logging.error(e)
+            logger.error(e)
         # Commit changes
         conn.commit()
         # Close the database connection
         conn.close()
     except Error as e:
-        logging.error(e)
+        logger.error(e)
 
 def get_config(database_file='db/PiControl.db'):
     '''
@@ -138,9 +139,9 @@ def get_config(database_file='db/PiControl.db'):
             conn.close()
             return (config)
         except Error as (e):
-            logging.error(e)
+            logger.error(e)
     except Error as e:
-        logging.error(e)
+        logger.error(e)
 
 def get_nodes(database_file='db/PiControl.db'):
     nodes = []
@@ -161,7 +162,7 @@ def get_nodes(database_file='db/PiControl.db'):
                         "last_checkin": datetime.datetime.fromtimestamp(row[4]).strftime('%c')
                     })
             except:
-                logging.error('nodes.append failed')
+                logger.error('nodes.append failed')
     except Error as (e):
-        logging.error(e)
+        logger.error(e)
     return jsonify({'nodes': nodes})
