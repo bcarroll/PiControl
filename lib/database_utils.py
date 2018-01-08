de667db1427f968292f600d478bb37887de39411
Brett Carroll <brettwcarroll@hotmail.com>
2018-01-08 14:27:01 -0500

    avoid cyclic import

--- lib/database_utils.py
+++ lib/database_utils.py
@@ -7,10 +7,10 @@ from time import time
 import datetime
 from pprint import pprint
 from flask import jsonify
-#import logging
-#from logging.handlers import RotatingFileHandler
+import logging
+from logging.handlers import RotatingFileHandler
 
-from lib.pi_utilities import pi_model
+from lib.pi_model import pi_model
 
 def create_database(database_file='db/PiControl.db'):
     '''
@@ -20,7 +20,7 @@ def create_database(database_file='db/PiControl.db'):
         database_file {str} -- Path to the database file to create (default: {'../db/PiControl.db'})
     '''
     try:
-        #logger.debug('create_database() called...')
+        logger.debug('create_database() called...')
         conn = sqlite3.connect(database_file)
         # Create PiControl database tables
         create_tables(conn)
@@ -29,7 +29,7 @@ def create_database(database_file='db/PiControl.db'):
         # Close the database connection
         conn.close()
     except Error as e:
-        #logger.error('Error opening/creating database. ' + e)
+        logging.error('Error opening/creating database. ' + e)
 
 def create_tables(connection):
     '''
@@ -39,13 +39,13 @@ def create_tables(connection):
         connection {sqlite3.Connection} -- A previously created sqlite3 database connection object
     '''
     try:
-        #logger.debug('create_tables() called...')
+        logger.debug('create_tables() called...')
         cursor = connection.cursor()
         cursor.execute("CREATE TABLE IF NOT EXISTS 'nodes' ('ipaddress' VARCHAR NOT NULL, 'hostname' VARCHAR NOT NULL, 'revision' VARCHAR NOT NULL, 'serialnumber' VARCHAR NOT NULL,'last_checkin' DATETIME NOT NULL)")
         cursor.execute("CREATE TABLE IF NOT EXISTS 'config' ('id' TEXT NOT NULL, 'beacon_port' INTEGER NOT NULL, 'beacon_interval' INTEGER NOT NULL, 'secret_key' TEXT NOT NULL, 'log_level' INTEGER NOT NULL, 'log_file' TEXT NOT NULL, 'log_files_backup' INTEGER NOT NULL, 'log_file_size' INTEGER NOT NULL)")
         create_config(cursor)
     except Error as e:
-        #logger.error(e)
+        logger.error(e)
 
 def create_config(cursor):
     '''
@@ -55,7 +55,7 @@ def create_config(cursor):
     Arguments:
         cursor {sqlite3.Cursor} -- A previsouly created sqlite3 cursor object
     '''
-    #logger.debug('create_config() called...')
+    logger.debug('create_config() called...')
     # Get all rows from the config database table
     cursor.execute("SELECT * FROM 'config'")
     #There should never be more than one row in the config table
@@ -64,17 +64,17 @@ def create_config(cursor):
     try:
         # If the config table already has data, we don't need to do anything
         t = config_rows[0]
-        #logger.debug('config table previously initialized.')
+        logger.debug('config table previously initialized.')
     except:
         # No results.  Add the default configuration data
-        #logger.info('Adding default configuration to the PiControl database.')
+        logger.info('Adding default configuration to the PiControl database.')
         try:
             cursor.execute("INSERT INTO 'config' ('id', 'beacon_port', 'beacon_interval', 'secret_key', 'log_level', 'log_file', 'log_files_backup', 'log_file_size') VALUES ('active', 31415, 60, 'PiControl', 10, 'logs/PiControl.log', 5, 4096000)")
         except:
-            #logger.error('Error adding default configuration to PiControl database. ')
+            logger.error('Error adding default configuration to PiControl database. ')
 
 def update_node(ipaddress, hostname, revision, serialnumber, last_checkin, database_file='db/PiControl.db'):
-    #logger.debug("Updating node: ipaddress=" + str(ipaddress) + ", hostname=" + str(hostname) + ", revision=" + str(revision) + ", serialnumber=" + str(serialnumber) +", last_checkin=" + str(last_checkin))
+    logger.debug("Updating node: ipaddress=" + str(ipaddress) + ", hostname=" + str(hostname) + ", revision=" + str(revision) + ", serialnumber=" + str(serialnumber) +", last_checkin=" + str(last_checkin))
     if ipaddress == '127.0.0.1':
         return
     try:
@@ -85,50 +85,50 @@ def update_node(ipaddress, hostname, revision, serialnumber, last_checkin, datab
             results = cursor.fetchone()
             if results:
                 try:
-                    #logger.debug('Updating node (' + hostname + ':' + ipaddress + ') in nodes table.')
+                    logger.debug('Updating node (' + hostname + ':' + ipaddress + ') in nodes table.')
                     cursor.execute('UPDATE nodes set ipaddress={0}, hostname={1}, revision={2}, serialnumber={3}, last_checkin={4} WHERE ipaddress={0}'.format("'{}'".format(ipaddress), "'{}'".format(hostname), "'{}'".format(revision), "'{}'".format(serialnumber), last_checkin))
                 except:
-                    #logger.error('Error updating node (' + str(hostname) + ':' + str(ipaddress) + ') in nodes table.')
+                    logger.error('Error updating node (' + str(hostname) + ':' + str(ipaddress) + ') in nodes table.')
                     return()
             else:
                 try:
-                    #logger.info('Node (' + hostname + ':' + ipaddress + ') does not exist in nodes table.  Inserting...')
+                    logger.info('Node (' + hostname + ':' + ipaddress + ') does not exist in nodes table.  Inserting...')
                     cursor.execute('INSERT INTO nodes (ipaddress, hostname, revision, serialnumber, last_checkin) VALUES ({0},{1},{2},{3},{4})'.format("'{}'".format(ipaddress), "'{}'".format(hostname), "'{}'".format(revision), "'{}'".format(serialnumber), last_checkin))
                 except:
-                    #logger.error('Error inserting node (' + str(hostname) + ':' + str(ipaddress) + ') in nodes table.')
+                    logger.error('Error inserting node (' + str(hostname) + ':' + str(ipaddress) + ') in nodes table.')
                     return()
         except Error as (e):
-            #logger.error(e)
+            logger.error(e)
             return()
         # Commit changes
         conn.commit()
         # Close the database connection
         conn.close()
     except Error as e:
-        #logger.error(e)
+        logger.error(e)
         return()
 
 def update_config(beacon_port, beacon_interval, secret_key, log_level, log_file, log_files_backup, log_file_size, database_file='db/PiControl.db'):
-    #logger.debug('Updating config: beacon_port=' + str(beacon_port) + ', beacon_interval=' + str(beacon_interval) + ', secret_key=' + str(secret_key) + ', log_level=' + str(log_level) + ', log_file=' + str(log_file) + ', log_files_backup=' + str(log_files_backup) + ', log_file_size=' + str(log_file_size))
+    logger.debug('Updating config: beacon_port=' + str(beacon_port) + ', beacon_interval=' + str(beacon_interval) + ', secret_key=' + str(secret_key) + ', log_level=' + str(log_level) + ', log_file=' + str(log_file) + ', log_files_backup=' + str(log_files_backup) + ', log_file_size=' + str(log_file_size))
     try:
         conn = sqlite3.connect(database_file)
         cursor = conn.cursor()
         try:
             cursor.execute('UPDATE config set (beacon_port={0}, beacon_interval={1}, secret_key={2}, log_level={3}, log_file={4}, log_files_backup={5}, log_file_size={6}) WHERE id="active"'.format(beacon_port, beacon_interval, "'{}'".format(secret_key), log_level, "'{}'".format(log_file), log_files_backup, log_file_size))
         except Error as (e):
-            #logger.error(e)
+            logger.error(e)
         # Commit changes
         conn.commit()
         # Close the database connection
         conn.close()
     except Error as e:
-        #logger.error(e)
+        logger.error(e)
 
 def get_config(database_file='db/PiControl.db'):
     '''
     Returns PiControl configuration (from the PiControl database) in JSON
     '''
-    #logger.debug('get_config() called...')
+    logger.debug('get_config() called...')
     try:
         conn = sqlite3.connect(database_file)
         cursor = conn.cursor()
@@ -148,12 +148,12 @@ def get_config(database_file='db/PiControl.db'):
             conn.close()
             return (config)
         except Error as (e):
-            #logger.error(e)
+            logger.error(e)
     except Error as e:
-        #logger.error(e)
+        logger.error(e)
 
 def get_nodes(database_file='db/PiControl.db'):
-    #logger.debug('get_nodes() called...')
+    logger.debug('get_nodes() called...')
     nodes = []
     try:
         conn = sqlite3.connect(database_file)
@@ -172,44 +172,44 @@ def get_nodes(database_file='db/PiControl.db'):
                         "last_checkin": datetime.datetime.fromtimestamp(row[4]).strftime('%c')
                     })
             except:
-                #logger.error('nodes.append failed')
+                logger.error('nodes.append failed')
     except Error as (e):
-        #logger.error(e)
+        logger.error(e)
     return jsonify({'nodes': nodes})
 
 
-# ###############################################################################
-# # Logging workaround
-# log_level        = 10
-# log_file         = "logs/PiControl_default.log"
-# log_format       = '[%(asctime)s][%(levelname)s][%(thread)s][%(name)s] %(message)s'
-# log_files_backup = 5
-# log_file_size    = 4096000
+###############################################################################
+# Logging workaround
+log_level        = 10
+log_file         = "logs/PiControl_default.log"
+log_format       = '[%(asctime)s][%(levelname)s][%(thread)s][%(name)s] %(message)s'
+log_files_backup = 5
+log_file_size    = 4096000
 
-# #######################################################################
-# #Setup logging
-# logger    = logging.getLogger(__name__)
-# logformat = logging.Formatter('[%(asctime)s][%(levelname)s][%(thread)s][%(name)s] %(message)s')
+#######################################################################
+#Setup logging
+logger    = logging.getLogger(__name__)
+logformat = logging.Formatter('[%(asctime)s][%(levelname)s][%(thread)s][%(name)s] %(message)s')
 
-# loglevels = {
-#     50: logging.CRITICAL,
-#     40: logging.ERROR,
-#     30: logging.WARNING,
-#     20: logging.INFO,
-#     10: logging.DEBUG,
-#     0: "NONE"
-# }
+loglevels = {
+    50: logging.CRITICAL,
+    40: logging.ERROR,
+    30: logging.WARNING,
+    20: logging.INFO,
+    10: logging.DEBUG,
+    0: "NONE"
+}
 
-# # Set the logging level
-# loglevel = ( loglevels[log_level] )
+# Set the logging level
+loglevel = ( loglevels[log_level] )
 
-# handler = RotatingFileHandler(log_file, mode='a', maxBytes=log_file_size, backupCount=log_files_backup)
+handler = RotatingFileHandler(log_file, mode='a', maxBytes=log_file_size, backupCount=log_files_backup)
 
-# if log_level == 0:
-#     #Logging is disabled
-#     handler = logging.NullHandler()
-#     print ('Logging is disabled')
+if log_level == 0:
+    #Logging is disabled
+    handler = logging.NullHandler()
+    print ('Logging is disabled')
 
-# handler.setFormatter(logformat)
-# logger.addHandler(handler)
-# logger.setLevel(loglevel)
+handler.setFormatter(logformat)
+logger.addHandler(handler)
+logger.setLevel(loglevel)
---
+++ lib/pi_model.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+
+import os
+from flask import jsonify
+from lib._logging import logger
+
+def pi_revision():
+    '''
+    Return the Raspberry Pi revision number
+    https://www.raspberrypi-spy.co.uk/2012/09/checking-your-raspberry-pi-board-version/
+    '''
+    try:
+        revision = os.popen("cat /proc/cpuinfo|grep '^Revision'|awk '{print $3}'").read().splitlines()
+    except:
+        logger.error('Error getting pi_revision')
+    try:
+        revision_string = revision[0]
+    except:
+        revision_string = ['ERROR']
+    return(revision_string)
+
+def pi_serialnumber(type='JSON'):
+    '''
+    Return the Raspberry Pi serial number
+    '''
+    try:
+        serialnumber = os.popen("cat /proc/cpuinfo|grep '^Serial'|awk '{print $3}'").read().splitlines()
+    except:
+        logger.error('Error getting pi_serialnumber')
+    if type == 'JSON':
+        return jsonify(serialnumber=serialnumber)
+    else:
+        try:
+            serialnumber_string = serialnumber[0]
+        except:
+            serialnumber_string = 'ERROR'
+        return(serialnumber_string)
+
+def pi_model(revision, type='JSON'):
+    pi = {}
+    pi['0002']   = {"model": "Model B v1.0","ram": "256MB"}
+    pi['0003']   = {"model": "Model B v1.0","ram": "256MB"}
+    pi['0004']   = {"model": "Model B v2.0","ram": "256MB"}
+    pi['0005']   = {"model": "Model B v2.0","ram": "256MB"}
+    pi['0006']   = {"model": "Model B v2.0","ram": "256MB"}
+    pi['0007']   = {"model": "Model A v2.0","ram": "256MB"}
+    pi['0008']   = {"model": "Model A v2.0","ram": "256MB"}
+    pi['0009']   = {"model": "Model A v2.0","ram": "256MB"}
+    pi['0010']   = {"model": "Model B+ v1.0","ram": "512MB"}
+    pi['0011']   = {"model": "Compute Module v1.0","ram": "512MB"}
+    pi['0012']   = {"model": "Model A+ v1.1","ram": "256MB"}
+    pi['0013']   = {"model": "Model B+ v1.2","ram": "512MB"}
+    pi['0014']   = {"model": "Compute Module v1.0","ram": "512MB"}
+    pi['0015']   = {"model": "Model A+ v1.1","ram": "256MB"}
+    pi['000d']   = {"model": "Model B v2.0","ram": "512MB"}
+    pi['000e']   = {"model": "Model B v2.0","ram": "512MB"}
+    pi['000f']   = {"model": "Model B v2.0","ram": "512MB"}
+    pi['900021'] = {"model": "Model A+ v1.1","ram": "512MB"}
+    pi['900032'] = {"model": "Model B+ v1.2","ram": "512MB"}
+    pi['900092'] = {"model": "Zero v1.2","ram": "512MB"}
+    pi['900093'] = {"model": "Zero v1.3","ram": "512MB"}
+    pi['9000C1'] = {"model": "Zero W v1.1","ram": "512MB"}
+    pi['920093'] = {"model": "Zero v1.3","ram": "512MB"}
+    pi['a01040'] = {"model": "2 Model B v1.0","ram": "1GB"}
+    pi['a01041'] = {"model": "2 Model B v1.1","ram": "1GB"}
+    pi['a02082'] = {"model": "3 Model B v1.2","ram": "1GB"}
+    pi['a020a0'] = {"model": "Compute Module 3 v1.0","ram": "1GB"}
+    pi['a21041'] = {"model": "2 Model B v1.1","ram": "1GB"}
+    pi['a22042'] = {"model": "2 Model B v1.2","ram": "1GB"}
+    pi['a22082'] = {"model": "3 Model B v1.2","ram": "1GB"}
+    pi['a32082'] = {"model": "3 Model B v1.2","ram": "1GB"}
+    if type == 'JSON':
+        return jsonify(pi[revision])
+    else:
+        return(pi[revision])
