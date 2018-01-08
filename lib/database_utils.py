70a8b3171c9f295f227f5b8da80f432a578812c7
Brett Carroll <brettwcarroll@hotmail.com>
2018-01-08 09:56:04 -0500

    debugging

--- lib/database_utils.py
+++ lib/database_utils.py
@@ -24,7 +24,7 @@ def create_database(app_dir='./'):
     try:
         conn = sqlite3.connect(database_file)
         # Create PiControl database tables
-        create_tables(conn, app_dir, database_file)
+        create_tables(conn, app_dir)
         # Commit changes
         conn.commit()
         # Close the database connection
@@ -44,7 +44,7 @@ def create_tables(connection, app_dir):
         cursor = connection.cursor()
         cursor.execute("CREATE TABLE IF NOT EXISTS 'nodes' ('ipaddress' VARCHAR NOT NULL, 'hostname' VARCHAR NOT NULL, 'revision' VARCHAR NOT NULL, 'serialnumber' VARCHAR NOT NULL,'last_checkin' DATETIME NOT NULL)")
         cursor.execute("CREATE TABLE IF NOT EXISTS 'config' ('id' TEXT NOT NULL, 'app_dir' TEXT NOT NULL, 'database_file' TEXT NOT NULL, beacon_port' INTEGER NOT NULL, 'beacon_interval' INTEGER NOT NULL, 'secret_key' TEXT NOT NULL, 'log_level' INTEGER NOT NULL, 'log_file' TEXT NOT NULL, 'log_files_backup' INTEGER NOT NULL, 'log_file_size' INTEGER NOT NULL)")
-        create_config(cursor, app_dir, database_file)
+        create_config(cursor, app_dir)
     except Error as e:
         logger.error(e)
 
