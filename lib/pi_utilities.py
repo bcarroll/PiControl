402e836c447bbb3617fb6c98ad513e78e87a427b
Brett Carroll <brettwcarroll@hotmail.com>
2018-01-07 23:37:02 -0500

    logging

--- lib/pi_utilities.py
+++ lib/pi_utilities.py
@@ -6,7 +6,8 @@ import subprocess
 from flask import jsonify
 import psutil
 
-from lib._logging import logger
+import logging
+from logging.handlers import RotatingFileHandler
 
 ########################################################################
 # vcgencmd command references:
@@ -232,3 +233,57 @@ def process_list():
 def service_status():
     #services = os.popen("for svc in `service --status-all|awk '{print $4}'`;do service $svc status|grep -v \"Warning: Unit\";done").read().splitlines()
     pass
+
+###############################################################################
+# Logging workaround
+log_level        = 10
+log_file         = "logs/PiControl_default.log"
+log_format       = '[%(asctime)s][%(levelname)s][%(thread)s][%(name)s] %(message)s'
+log_files_backup = 5
+log_file_size    = 4096000
+
+try:
+    config            = get_config()
+    log_level         = int(config['log_level'])
+    log_file          = str(config['log_file'])
+    #log_format       = str(config['log_format'])
+    #log_files_backup = int(config['log_files_backup'])
+    #log_role_size    = int(config['log_file_size'])
+except:
+    logging.error('Error getting configuration from PiControl database')
+
+#Create log directory if it does not already exist
+LOG_DIR = "logs"
+if not os.path.exists(LOG_DIR):
+    try:
+        os.makedirs(LOG_DIR)
+    except PermissionError:
+        sys.exit("Error creating " + LOG_DIR + '. PERMISSION DENIED')
+
+#######################################################################
+#Setup logging
+logger    = logging.getLogger(__name__)
+logformat = logging.Formatter('[%(asctime)s][%(levelname)s][%(thread)s][%(name)s] %(message)s')
+
+loglevels = {
+    50: logging.CRITICAL,
+    40: logging.ERROR,
+    30: logging.WARNING,
+    20: logging.INFO,
+    10: logging.DEBUG,
+    0: "NONE"
+}
+
+# Set the logging level
+loglevel = ( loglevels[log_level] )
+
+handler = RotatingFileHandler(log_file, mode='a', maxBytes=log_file_size, backupCount=log_files_backup)
+
+if log_level == 0:
+    #Logging is disabled
+    handler = logging.NullHandler()
+    print ('Logging is disabled')
+
+handler.setFormatter(logformat)
+logger.addHandler(handler)
+logger.setLevel(loglevel)
