63166c65fd544d84667b48f599eb041268fc6157
Brett Carroll <brettwcarroll@hotmail.com>
2018-01-08 12:16:23 -0500

    update

--- PiControl.sh
+++ PiControl.sh
@@ -3,20 +3,20 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${SCRIPT_DIR}
 
 start(){
+    echo "Starting PiControl"
     python PiControl.py
 }
 
 stop(){
+    echo "Stopping PiControl"
     sudo ps -ef|grep PiControl|xargs kill
 }
 
 restart(){
+    echo "Restarting PiControl"
     stop()
     counter=1
-    while [$counter -le 5]
-    do
-        sleep(1)
-        ((counter++))
+    sleep(3)
     done
     start()
 }
