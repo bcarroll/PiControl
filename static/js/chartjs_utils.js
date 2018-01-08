9e52a5403725070c3653654c62f6ed9cf33caa18
Brett Carroll <brettwcarroll@hotmail.com>
2018-01-08 17:48:07 -0500

    implement chart,js

---
+++ static/js/chartjs_utils.js
@@ -0,0 +1,14 @@
+function addChartData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+function removeChartData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
