055d071acca3d49ab6615a9786364446b6f11ff5
Brett Carroll <brettwcarroll@hotmail.com>
2018-01-09 20:53:25 -0500

    added datetime labels

--- lib/chart_utils.py
+++ lib/chart_utils.py
@@ -2,22 +2,33 @@
 import threading
 from time import sleep
 
+from datetime import datetime
+
 from lib._logging import logger
 from lib.pi_utilities import cpu_temperature
 
 class PiControlChart():
     instances = []
-    def __init__(self, data_function, args=None, interval=5, max_elements=60, fill_empty=True, fill_data=0):
+    def __init__(self, data_function=None, args=None, labels='datetime', datetime_format='%b %d %I:%M:%S %p', interval=5, max_elements=60, fill_empty=True, fill_data=0):
         '''
         Arguments:
-            data_function {function} -- Name of a function that will return data to be added to the chart
-            interval {integer} -- Seconds between chart data updates
-            max_elements {integer} -- Maximum number of elements in the list
+            data_function {function} -- Name of a function that will return data to be added to the chart (default: None)
+            labels {string|None} -- Add labels to the data with a formatted datetime string (default: 'datetime')
+            datetime_format {string} -- datetime format in strftime format (default: '%b %d %I:%M:%S %p')
+            interval {integer} -- Seconds between chart data updates (default: 5)
+            max_elements {integer} -- Maximum number of elements in the list (default: 60)
+            fill_empty {boolean} -- Fill empty datapoints with data, when the chart is created (default: True)
+            fill_data {data} -- Data to fill empty data points with (default: 0)
+
         '''
         self.function     = data_function
         self.function_args= args
+        self.labels       = labels
+        self.date_format  = datetime_format
         self.interval     = interval
         self.list         = []
+        if labels == 'datetime':
+            self.labels   = []
         self.max_elements = max_elements
         self.fill_empty   = fill_empty
         self.fill_data    = fill_data
@@ -65,10 +76,22 @@ class PiControlChart():
             fill_empty {boolean} -- Fill empty elements in the list with fill_data
             fill_data {any} -- Value to use when filling the list
         '''
+        # Fill beginning of the list
         logger.debug('addData(' + str(data) + ') called')
         if self.fill_empty and self.max_elements > len(self.list):
+            previous_label_offset = self.interval
             while self.max_elements-1 > len(self.list):
                 self.list.append(self.fill_data)
+                if self.labels:
+                    # fill the list with time labels
+                    self.labels.append(time.strftime( self.datetime_format, time.localtime( int(time.time()) - previous_label_offset ) ))
+                    previous_label_offset = previous_label_offset + self.interval
+
         if len(self.list) == self.max_elements:
             self.list.pop(0)
+            if self.labels:
+                self.labels.pop(0)
+        # Add new data to the list
         self.list.append(data)
+        self.labels.append( time.strftime( self.datetime_format, time.localtime( int( time.time() ) ) ) )
+
