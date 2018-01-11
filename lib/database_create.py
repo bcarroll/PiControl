f6fdbf8e3615e39a7e7c0f58a1ad9ec368bf59cd
Brett Carroll <brettwcarroll@hotmail.com>
2018-01-11 18:18:01 -0500

    specify secret key on first run
    User is prompted for a secret key when the PiControl database is created

--- lib/database_create.py
+++ lib/database_create.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 
 def create_database(database_file='db/PiControl.db'):
@@ -7,11 +8,17 @@ def create_database(database_file='db/PiControl.db'):
     Keyword Arguments:
         database_file {str} -- Path to the database file to create (default: {'../db/PiControl.db'})
     '''
-    try:
+    if os.path.isfile(database_file):
         print('Creating PiControl database')
+        print('If this is the first (or only) instance of PiControl on the network, enter a new passphrase to be used as the PiControl Secret Key.')
+        print('If you have already created a PiControl Secret Key passphrase on another Raspberry Pi on your network, enter that passphrase to allow PiControl to talk to the other device(s).')
+        secret_key = raw_input('Enter your PiControl Secret Key: ')
+        while  secret_key == '':
+            secret_key = raw_input('A Secret Key is required. Enter your PiControl Secret Key: ')
+    try:
         conn = sqlite3.connect(database_file)
         # Create PiControl database tables
-        create_tables(conn)
+        create_tables(conn, secret_key)
         # Commit changes
         conn.commit()
         # Close the database connection
@@ -19,7 +26,7 @@ def create_database(database_file='db/PiControl.db'):
     except Exception as e:
         print('Error opening/creating database. ' + e.message)
 
-def create_tables(connection):
+def create_tables(connection, secret_key):
     '''
     Create PiControl database tables using the provided database connection
 
@@ -31,11 +38,11 @@ def create_tables(connection):
         cursor = connection.cursor()
         cursor.execute("CREATE TABLE IF NOT EXISTS 'nodes' ('ipaddress' VARCHAR NOT NULL, 'hostname' VARCHAR NOT NULL, 'revision' VARCHAR NOT NULL, 'serialnumber' VARCHAR NOT NULL,'last_checkin' DATETIME NOT NULL)")
         cursor.execute("CREATE TABLE IF NOT EXISTS 'config' ('id' TEXT NOT NULL, 'beacon_port' INTEGER NOT NULL, 'beacon_interval' INTEGER NOT NULL, 'secret_key' TEXT NOT NULL, 'log_level' INTEGER NOT NULL, 'log_file' TEXT NOT NULL, 'log_files_backup' INTEGER NOT NULL, 'log_file_size' INTEGER NOT NULL, 'beacon_listener_enabled' INTEGER NOT NULL,'beacon_sender_enabled' INTEGER NOT NULL, 'background_charts_enabled' INTEGER NOT NULL)")
-        create_config(cursor)
+        create_config(cursor, secret_key)
     except Exception as e:
         print(e.message)
 
-def create_config(cursor):
+def create_config(cursor, secret_key):
     '''
     Insert default configuration to config table
     Silently catches the exception if the config data already exists
@@ -56,7 +63,7 @@ def create_config(cursor):
         # No results.  Add the default configuration data
         print('Adding default configuration to the PiControl database.')
         try:
-            cursor.execute("INSERT INTO 'config' ('id', 'beacon_port', 'beacon_interval', 'secret_key', 'log_level', 'log_file', 'log_files_backup', 'log_file_size', 'beacon_listener_enabled', 'beacon_sender_enabled', 'background_charts_enabled') VALUES ('active', 31415, 60, 'PiControl', 10, 'logs/PiControl.log', 5, 4096000, 1, 1, 1)")
+            cursor.execute("INSERT INTO 'config' ('id', 'beacon_port', 'beacon_interval', 'secret_key', 'log_level', 'log_file', 'log_files_backup', 'log_file_size', 'beacon_listener_enabled', 'beacon_sender_enabled', 'background_charts_enabled') VALUES ('active', 31415, 60, " + secret_key + ", 10, 'logs/PiControl.log', 5, 4096000, 1, 1, 1)")
         except Exception as e:
             print('Error adding default configuration to PiControl database. ' + e.message)
 
