1a6c5e2e2bf401ca48268f561ff4a8c8c91f7251
Brett Carroll <brettwcarroll@hotmail.com>
2018-01-12 11:59:44 -0500

    update

--- lib/node_utils.py
+++ lib/node_utils.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-
+import socket
 import requests
 import json
 from flask import jsonify
@@ -8,27 +8,34 @@ from lib.database_utils import get_nodes
 from lib._logging import logger
 
 def node_cpu_usage():
+    get_data_from_nodes('/dashboard/cpu_usage')
+    return jsonify({'cpu_usage': node_data})
+
+def get_data_from_nodes(URI):
     nodes = get_nodes(type='list')
+    # Add local node to list of nodes
+    nodes.append({"ipaddress": '127.0.0.1', "hostname": socket.gethostname(), "last_checkin": ""})
     node_data = []
+    node_data.append(node_chart_data)
+    # Add data from discovered nodes
     for node in nodes:
-        print(node['ipaddress'] + ', ' + node['hostname'] + ', ' + node['last_checkin'])
-        node_url = 'https://' + node['ipaddress'] + ':31415/dashboard/cpu_usage'
+        node_url = 'https://' + node['ipaddress'] + ':31415' + URI
         try:
             r = requests.get(node_url, verify=False)
             if r.status_code != 200:
                 logger.warn(node_url + ' returned ' + r.status_code)
-            node_cpu_usage_data = {
+            node_chart_data = {
                     "ipaddress": node['ipaddress'],
                     "hostname": node['hostname'],
                     "last_checkin": node['last_checkin'],
                     "data": r.json()
                 }
-            node_data.append(node_cpu_usage_data)
+            node_data.append(node_chart_data)
         except:
             logger.warn('Error accessing ' + node_url)
-            node_cpu_usage_data = {
+            node_chart_data = {
                     node['ipaddress']: ""
                 }
             node_data.append(node_cpu_usage_data)
-    return jsonify({'cpu_usage': node_data})
+    return(node_data)
 
