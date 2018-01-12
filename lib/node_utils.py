bcd96390c72efa20f096a0d54c9eaf946f3f507f
Brett Carroll <brettwcarroll@hotmail.com>
2018-01-12 11:17:35 -0500

    debugging

--- lib/node_utils.py
+++ lib/node_utils.py
@@ -5,14 +5,15 @@ import requests
 from flask import jsonify
 
 from lib.database_utils import get_nodes
+from lib._logging import logger
 
 def node_cpu_usage():
     nodes = get_nodes(type='list')
     node_data = []
     for node in nodes:
         print(node['ipaddress'] + ', ' + node['hostname'] + ', ' + node['last_checkin'])
+        node_url = 'https://' + node['ipaddress'] + ':31415/dashboard/cpu_usage'
         try:
-            node_url = 'https://' + node['ipaddress'] + ':31415/dashboard/cpu_usage'
             r = requests.get(node_url)
             if r.status_code != 200:
                 logger.warn(node_url + ' returned ' + r.status_code)
@@ -21,8 +22,9 @@ def node_cpu_usage():
                 }
             node_data.append(node_cpu_usage_data)
         except:
+            logger.warn('Error accessing ' + node_url)
             node_cpu_usage_data = {
-                    node['ipaddress']: r.text
+                    node['ipaddress']: ""
                 }
             node_data.append(node_cpu_usage_data)
     return jsonify({'cpu_usage': node_data})
